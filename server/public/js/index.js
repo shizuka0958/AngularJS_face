c9df6fd4919c5329001400213d774e89c1fa12bb
gc_jianshen <gc_jianshen@gagc.tv>
2018-06-13 11:31:54 +0800

    更新路由

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -1,15 +1,44 @@
-var app = angular.module('myApp',[]);
-app.controller('indexController',['$scope','$http',function($scope,$http){
+var app = angular.module('myApp', ['ngRoute']);
 
-    const socket = io.connect('http://localhost:8080');
-
-    socket.on('connect', function (data) {
-        socket.emit('get times');
-        socket.on('get times', function (data) {
-            console.log('get times:' + data.times); 
-            $scope.times = data.times+1;
-            console.log($scope.times);
-            $scope.$apply();
+app.run(['$rootScope',function($rootScope){
+    $rootScope.add=1
+    $rootScope.do = function(mark){
+        const socket = io.connect('http://localhost:8080');
+        socket.on('connect', function (data) {
+            socket.emit('get times');
+            socket.on('get times', function (data) {
+                console.log('get times:' + data.times); 
+                $rootScope.times = data.times+1+','+mark;
+                console.log($rootScope.times);
+                $rootScope.$apply();
+            });
         });
-    });
+    }
+    
+}]);
+
+app.config(["$routeProvider", function($routeProvider) {
+    $routeProvider.when("/add", {
+        templateUrl: 'add.html',
+    }).when("/detail", {
+        templateUrl: "detail.html"
+    }).when("/warn", {
+        templateUrl: "warn.html"
+    })
+}]);
+
+app.controller('indexController',['$scope','$http',function($scope,$http){
+    $scope.do(1)
+}])
+
+app.controller('detailController',['$scope','$http',function($scope,$http){
+    $scope.do(2)
+    
+
+}])
+
+app.controller('warnController',['$scope','$http',function($scope,$http){
+    $scope.do(3)
+    
+
 }])
